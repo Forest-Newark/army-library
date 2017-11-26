b9163443754ace3c06fee1c6741e8f915e8778b9
Forest <forest.newark@gmail.com>
2017-11-26 14:40:55 -0500

    updated controller with CRUD operations

--- Army-Library-Service/src/main/java/com/forestnewark/Library/Manager/controller/RestController.java
+++ Army-Library-Service/src/main/java/com/forestnewark/Library/Manager/controller/RestController.java
@@ -2,12 +2,24 @@ package com.forestnewark.Library.Manager.controller;
 
 import com.forestnewark.Library.Manager.Repository.CompositionRepository;
 import com.forestnewark.Library.Manager.bean.Composition;
+import com.forestnewark.Library.Manager.service.FileService;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.List;
+
+
 
 /**
  * Created by Forest on 11/25/17.
@@ -18,6 +30,9 @@ public class RestController {
 
     @Autowired
     CompositionRepository compositionRepository;
+
+    @Autowired
+    FileService fileService;
 
 
     /**
@@ -35,10 +50,78 @@ public class RestController {
      * Get all compositions
      * @return all compositions (unsorted)
      */
-    @RequestMapping("/compositions")
+    @RequestMapping("/composition")
     public List<Composition> getAllCompositions(){
         return compositionRepository.findAll();
     }
 
 
+
+
+    /**
+     * Add New Composition
+     * @param composition
+     * @return added composition
+     */
+    @RequestMapping(value="/composition/add", method= RequestMethod.POST)
+    public Composition addNewComposition(Composition composition){
+        compositionRepository.save(composition);
+        return composition;
+
+    }
+
+    /**
+     * Edit composition
+     * @param composition
+     * @return
+     */
+    @RequestMapping(value="/composition/update", method = RequestMethod.POST)
+    public Composition editComposition(@RequestBody Composition composition){
+        compositionRepository.save(composition);
+        return composition;
+    }
+
+
+    /**
+     * Delete Composition
+     * @param composition
+     * @return
+     */
+    @RequestMapping(value="/composition/delete", method= RequestMethod.POST)
+    public Composition deleteComposition(@RequestBody Composition composition){
+        compositionRepository.delete(composition);
+        return composition;
+    }
+
+
+    /**
+     * Upload CSV File
+     * @param file
+     * @throws IOException
+     */
+    @RequestMapping(value = "/submitCSV", method = RequestMethod.POST)
+    public void uploadFileHandler(@RequestParam("file") MultipartFile file) throws IOException {
+
+
+        Reader in = new FileReader(fileService.convert(file));
+
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader("Catagory", "libnum", "Title","Composer","Arranger","Copyright","Ensemble","Notes").parse(in);
+        for (CSVRecord record : records) {
+            Composition composition = new Composition(
+                    null,
+                    record.get("Catagory"),
+                    Integer.parseInt(record.get("libnum")),
+                    record.get("Title"),
+                    record.get("Composer"),
+                    record.get("Arranger"),
+                    record.get("Ensemble"),
+                    Integer.parseInt(record.get("Copyright")),
+                    record.get("Notes")
+            );
+            compositionRepository.save(composition);
+        }
+    }
+
+
+    
 }
